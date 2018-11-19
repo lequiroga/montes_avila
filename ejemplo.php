951ab4fbef0a4131fadcd6b7a410645bf119328d
luis quiroga <lequiroga@gmail.com>
2018-11-18 19:21:34 -0500

    g

--- ejemplo.php
+++ ejemplo.php
@@ -2,7 +2,7 @@
 
 // Conectando y seleccionado la base de datos
 function conectarBD(){
-   $dbconn = pg_connect("host=ec2-18-234-252-5.compute-1.amazonaws.com port=5432 dbname=clientes user=postgres password=postgres0417")
+   $dbconn = pg_connect("host=localhost port=5432 dbname=clientes user=postgres password=postgres0417")
                         or die('No se ha podido conectar: ' . pg_last_error());
    return $dbconn;
 }
